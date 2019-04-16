e99c31e60e2c606f860b13c31fd7cc2cd3b12c28
lukevandekieft <vandekie@gmail.com>
2019-04-16 16:31:46 -0700

    Finish Login page with routing, loading animations, and persistent login sessions.

--- notes-app-client/src/App.js
+++ notes-app-client/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import { LinkContainer } from "react-router-bootstrap";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
+import { Auth } from "aws-amplify";
 
 import "./App.css";
 import Routes from "./Routes";
@@ -11,25 +12,44 @@ class App extends Component {
     super(props);
 
     this.state = {
-      isAuthenticated: false
+      isAuthenticated: false,
+      isAuthenticating: true,
     };
+  }
+
+  async componentDidMount() {
+    try {
+      await Auth.currentSession();
+      this.userHasAuthenticated(true);
+    }
+    catch(e) {
+      if (e !== 'No current user') {
+        alert(e);
+      }
+    }
+    this.setState({ isAuthenticating: false });
   }
 
   userHasAuthenticated = authenticated => {
     this.setState({ isAuthenticated: authenticated });
   }
 
-  handleLogout = event => {
+  handleLogout = async event => {
+    //clears login session
+    await Auth.signOut();
     this.userHasAuthenticated(false);
+    this.props.history.push("/login");
   }
 
   render() {
+    //packages props for easy passing
     const childProps = {
       isAuthenticated: this.state.isAuthenticated,
       userHasAuthenticated: this.userHasAuthenticated
     };
 
     return (
+      !this.state.isAuthenticating &&
       <div className="App container">
         <Navbar fluid collapseOnSelect>
           <Navbar.Header>
@@ -60,4 +80,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withRouter(App);
