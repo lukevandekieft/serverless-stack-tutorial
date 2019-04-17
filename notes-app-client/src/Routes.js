34d9f6362b4ee85def780ef31d03a1d3d416b57a
lukevandekieft <vandekie@gmail.com>
2019-04-17 11:57:56 -0700

    Set up protected routes with rerouting functionality.

--- notes-app-client/src/Routes.js
+++ notes-app-client/src/Routes.js
@@ -2,19 +2,21 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 
 import AppliedRoute from "./components/AppliedRoute";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
 import NewNote from "./containers/NewNote";
 import Notes from "./containers/Notes";
 import NotFound from "./containers/NotFound";
 import Signup from "./containers/Signup";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 
 export default ({ childProps }) =>
   <Switch>
     <AppliedRoute path="/" exact component={Home} props={childProps}/>
-    <AppliedRoute path="/login" exact component={Login} props={childProps}/>
-    <AppliedRoute path="/notes/new" exact component={NewNote} props={childProps} />
-    <AppliedRoute path="/notes/:id" exact component={Notes} props={childProps} />
-    <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
+    <UnauthenticatedRoute path="/login" exact component={Login} props={childProps} />
+    <UnauthenticatedRoute path="/signup" exact component={Signup} props={childProps} />
+    <AuthenticatedRoute path="/notes/new" exact component={NewNote} props={childProps} />
+    <AuthenticatedRoute path="/notes/:id" exact component={Notes} props={childProps} />
     <Route component={NotFound} />
   </Switch>;
