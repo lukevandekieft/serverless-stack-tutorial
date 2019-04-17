f3774f876daca42718e44761eeec288bcc1135a9
lukevandekieft <vandekie@gmail.com>
2019-04-17 11:40:32 -0700

    Display new notes and allow for editing & deleting.

--- notes-app-client/src/Routes.js
+++ notes-app-client/src/Routes.js
@@ -5,6 +5,7 @@ import AppliedRoute from "./components/AppliedRoute";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
 import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes";
 import NotFound from "./containers/NotFound";
 import Signup from "./containers/Signup";
 
@@ -13,6 +14,7 @@ export default ({ childProps }) =>
     <AppliedRoute path="/" exact component={Home} props={childProps}/>
     <AppliedRoute path="/login" exact component={Login} props={childProps}/>
     <AppliedRoute path="/notes/new" exact component={NewNote} props={childProps} />
+    <AppliedRoute path="/notes/:id" exact component={Notes} props={childProps} />
     <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
     <Route component={NotFound} />
   </Switch>;
